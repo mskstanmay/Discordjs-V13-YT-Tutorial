bd5ce93975b0ae562d6ccacb85dc33d7eba47aa8
Msks TanmaY <mskstanmay@gmail.com>
2021-08-09 16:35:33 +0530

    Grammer Fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-//Require Packages
+//Require Packages..
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 require('dotenv').config()
 
-//Events
+//Events.
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity('Youtube.', { type: 'WATCHING' });
 });
 
-//Login
+//Login.
 client.login(process.env.token);
